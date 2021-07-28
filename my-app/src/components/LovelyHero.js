73353d1701b303d1697583053ace521a1d8ce8f1
Пользователь <pashin.andrey94@gmail.com>
2021-07-28 17:08:40 +0700

    add fetch request

---
+++ my-app/src/components/LovelyHero.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class LovelyHero extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default LovelyHero;
